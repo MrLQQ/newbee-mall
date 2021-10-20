fd7631130c396c805c9907bc3891a8f9e4a3febe
MrLQQ <280235109@qq.com>
2021-10-20 19:58:13 +0800

    注释调了不必要的语句

--- src/main/resources/static/mall/js/search.js
+++ src/main/resources/static/mall/js/search.js
@@ -1,18 +1,19 @@
-$(function () {
-    $('#keyword').keypress(function (e) {
-        var key = e.which; //e.which是按键的值
-        if (key == 13) {
-            var q = $(this).val();
-            if (q && q != '') {
-                window.location.href = '/search?keyword=' + q;
-            }
-        }
-    });
-});
+//$(function () {
+//    $('#keyword').keypress(function (e) {
+//        var key = e.which; //e.which是按键的值
+//        if (key == 13) {
+//            var q = $(this).val();
+//            if (q && q != '') {
+//                window.location.href = '/search?keyword=' + q;
+//            }
+//        }
+//    });
+//});
 
+/*搜索按钮的onclick触发方法*/
 function search() {
     var q = $('#keyword').val();
     if (q && q != '') {
-        window.location.href = '/search?keyword=' + q;
+        window.location.href = '/search?keyword=' + q;  // 发送search请求，跳转到搜索结果页面
     }
 }
